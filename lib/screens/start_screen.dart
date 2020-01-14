15a3d258c1f42074dd3393ff155fb067b36a3bf8
Jacob Nyman <jacny980@student.liu.se>
2020-01-14 13:43:12 +0100

    Started creating a startscreen for the application

---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatefulWidget {
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Center(
+              child: Text('JACSA',
+                  style: TextStyle(color: Colors.amber, fontSize: 40)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 5),
+            child: Center(
+              child: Text('Trivia',
+                  style: TextStyle(color: Colors.amber, fontSize: 20)),
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {},
+                child: Text('Start Game',
+                    style: TextStyle(fontSize: 20, color: Colors.black26)),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text('High score',
+                    style: TextStyle(fontSize: 20, color: Colors.black26)),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                padding: EdgeInsets.symmetric(horizontal: 38),
+                child: Text('About',
+                    style: TextStyle(fontSize: 20, color: Colors.black26)),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                padding: EdgeInsets.symmetric(horizontal: 48),
+                child: Text('Exit',
+                    style: TextStyle(fontSize: 20, color: Colors.black26)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
+  }
+}
